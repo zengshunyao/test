f3bb99b5e0dbaf1aa55627c7279f4c26686553e1
zengshunyao <641995773@qq.com>
2018-08-20 00:22:16 +0800

    kafka first

---
+++ src/main/java/com/funi/distributedcomputer/kafka/MyKafkaProducer.java
@@ -0,0 +1,48 @@
+package com.funi.distributedcomputer.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class MyKafkaProducer {
+
+    private static final String KAFKA_BROKER_LIST = "47.52.33.73:9092";
+    private static final String TOPIC = "ONE";
+
+    private final Producer<Integer, String> producer;
+
+    private final String topic = TOPIC;
+
+    public MyKafkaProducer() {
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", KAFKA_BROKER_LIST);
+        properties.put("key.serializer", IntegerSerializer.class.getName());
+        properties.put("value.serializer", StringSerializer.class.getName());
+        properties.put("client.id", StringSerializer.class.getName());
+
+
+        this.producer = new KafkaProducer<Integer, String>(properties);
+    }
+
+    public void sendMsg() {
+        this.producer.send(new ProducerRecord<Integer, String>(this.topic, 1, "message"), new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                System.out.println("message send to [" + recordMetadata.partition() + "],offset[" + recordMetadata.offset() + "]");
+            }
+        });
+    }
+
+    public static void main(String[] args) throws IOException {
+//        System.out.println(IntegerSerializer.class.getName());
+
+        MyKafkaProducer producer = new MyKafkaProducer();
+        producer.sendMsg();
+
+        System.in.read();
+    }
+}
