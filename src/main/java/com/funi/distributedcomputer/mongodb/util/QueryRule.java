7a68e65011b6a9aaa462984cb2daa9524637f19e
zengshunyao <641995773@qq.com>
2018-09-18 23:30:45 +0800

    mongodb 1)mongo学习 java orm 框架封装1

---
+++ src/main/java/com/funi/distributedcomputer/mongodb/util/QueryRule.java
@@ -0,0 +1,43 @@
+package com.funi.distributedcomputer.mongodb.util;
+
+import java.util.List;
+
+public enum QueryRule {
+
+    BETWEEN, EQ, LIKE, NOT_EQ, GT, GE, LT, LE,
+    IN, NOT_IN, IS_NULL, IS_NOT_NULL, IS_EMPTY,
+    IS_NOT_EMPTY, ASC_ORDER, DESC_ORDER;
+
+
+    private List<Rule> ruleList;
+
+    public List<Rule> getRuleList() {
+        return ruleList;
+    }
+
+    public static class Rule {
+
+        private QueryRule type;
+        private String[] values;
+
+        private String andOr;
+
+        private String propertyName;
+
+        public QueryRule getType() {
+            return type;
+        }
+
+        public String[] getValues() {
+            return values;
+        }
+
+        public String getAndOr() {
+            return andOr;
+        }
+
+        public String getPropertyName() {
+            return propertyName;
+        }
+    }
+}
