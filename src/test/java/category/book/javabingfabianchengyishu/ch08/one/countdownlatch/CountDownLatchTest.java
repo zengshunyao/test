38436fcd412c9effedb149a78e153c63f90fb09f
zengshunyao <641995773@qq.com>
2019-03-21 18:41:24 +0800

    1

--- src/test/java/category/book/javabingfabianchengyishu/ch08/one/countdownlatch/CountDownLatchTest.java
+++ src/test/java/category/book/javabingfabianchengyishu/ch08/one/countdownlatch/CountDownLatchTest.java
@@ -25,44 +25,83 @@ public class CountDownLatchTest {
 
     //将count值减1
     //public void countDown() { };
+    final static Integer NUM = 4;
 
     public static void main(String[] args) {
-        final CountDownLatch latch = new CountDownLatch(2);
+
+        final CountDownLatch latch = new CountDownLatch(NUM);
 
         new Thread() {
+            private String name = "1";
+
             public void run() {
                 try {
-                    System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "正在执行");
                     Thread.sleep(3000);
-                    System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "执行完毕");
                     latch.countDown();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
-            ;
         }.start();
 
         new Thread() {
+            private String name = "2";
+
             public void run() {
                 try {
-                    System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "正在执行");
                     Thread.sleep(3000);
-                    System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "执行完毕");
                     latch.countDown();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
-            ;
         }.start();
+
+        new Thread() {
+            private String name = "3";
+
+            public void run() {
+                try {
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "正在执行");
+                    Thread.sleep(5000);
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "执行完毕");
+                    latch.countDown();
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "countDown");
+                    latch.await();
+                    System.out.println("子线程" + name + "------" + "over");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            private String name = "4";
+
+            public void run() {
+                try {
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "正在执行");
+                    Thread.sleep(5000);
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "执行完毕");
+                    latch.countDown();
+                    System.out.println("子线程" + name + "------" + Thread.currentThread().getName() + "countDown");
+                    latch.await();
+                    System.out.println("子线程" + name + "------" + "over");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
 
         try {
-            System.out.println("等待2个子线程执行完毕...");
+            System.out.println("等待" + NUM + "个子线程执行完毕...");
             latch.await();
-            System.out.println("2个子线程已经执行完毕");
+            System.out.println(NUM + "个子线程已经执行完毕");
             System.out.println("继续执行主线程");
         } catch (InterruptedException e) {
             e.printStackTrace();
