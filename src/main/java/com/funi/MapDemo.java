2f6df3bbc5513bb643ac9890d40c49a97970b59d
zengshunyao <641995773@qq.com>
2019-03-28 21:54:16 +0800

    https://www.jianshu.com/p/814432ed04e8

--- src/main/java/com/funi/MapDemo.java
+++ src/main/java/com/funi/MapDemo.java
@@ -1,14 +1,41 @@
 package com.funi;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class MapDemo {
 
-    public static void main(String[] args) {
-        Map map = new HashMap();
-        map.put("1",1);
-        map.put(1,"Jack");
-        System.out.println(map.get(1));
+    public static void main(String[] args) throws Exception {
+        //map内的数组容量大于等于64 且 链表数量大于8才会进行红黑树转换
+        HashMap map = new HashMap(64);
+        for (int i = 0; i <= 8; i++) {
+            map.put(new HashCodeOneObj(i), i);
+        }
+        System.out.println(map);
+    }
+
+    private static class HashCodeOneObj implements Comparable<HashCodeOneObj> {
+        private int val;
+
+        public HashCodeOneObj(int val) {
+            this.val = val;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        @Override
+        public int hashCode() {
+            //让所有数据都存入一个桶
+            return 1;
+        }
+
+        @Override
+        public int compareTo(HashCodeOneObj o) {
+            if (null == o) {
+                return -1;
+            }
+            return Integer.compare(this.getVal(), o.getVal());
+        }
     }
 }
