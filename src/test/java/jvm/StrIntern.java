d8a439496b3b44bcc1b5f78ca5d711a9a29c5c2f
zengshunyao <641995773@qq.com>
2020-03-17 01:51:00 +0800

    首次出现的坑

--- src/test/java/jvm/StrIntern.java
+++ src/test/java/jvm/StrIntern.java
@@ -69,7 +69,6 @@ public class StrIntern {
 
         String str1 = new StringBuilder("i'm").append(" T").toString();
         System.out.println(str1.intern() == str1);
-
         //
         String str2 = new StringBuilder("ja").append("va").toString();
         System.out.println(str2.intern() == str2);
