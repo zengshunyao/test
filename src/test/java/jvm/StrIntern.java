9922d799f2e65c369d5e60cfcec39f45417149bc
zengshunyao <641995773@qq.com>
2020-03-17 01:50:41 +0800

    首次出现的坑

--- src/test/java/jvm/StrIntern.java
+++ src/test/java/jvm/StrIntern.java
@@ -28,17 +28,16 @@ public class StrIntern {
 //        System.out.println(s3 == s4);
 
 
-
         //false,false,true
-        String s = new String("1");
-        String s2 = "1";
-        s.intern();
-        System.out.println(s == s2);
-        String s3 = new String("1") + new String("1");
-        String s4 = "11";
-        s3.intern();
-        System.out.println(s3 == s4);
-        System.out.println(s3.intern() == s4);
+//        String s = new String("1");
+//        String s2 = "1";
+//        s.intern();
+//        System.out.println(s == s2);
+//        String s3 = new String("1") + new String("1");
+//        String s4 = "11";
+//        s3.intern();
+//        System.out.println(s3 == s4);
+//        System.out.println(s3.intern() == s4);
 
         //+连接但编译器不优化
         //true,true
@@ -66,5 +65,25 @@ public class StrIntern {
 //        System.out.println(s1 == s1.intern());
 //        System.out.println(s1 + " " + s2);
 //        System.out.println(s2 == s1.intern());
+
+
+        String str1 = new StringBuilder("i'm").append(" T").toString();
+        System.out.println(str1.intern() == str1);
+
+        //
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);
+
+//        这段代码在JDK1.6中运行，会得到两个false，而在JDK1.7和1.8中运行会得到一个true和一个false。这个差异的原因是：
+//
+//        在JDK1.6中，intern()方法会把首次遇到的字符串实例复制到永久代中，返回的也是永久代中这个字符串实例的引用，
+//        而由StringBuilder创建的字符串实例在Java堆上，所以必然不是同一个引用。
+
+//        而JDK1.7中已经将运行时常量池从永久代移除，在Java 堆（Heap）中开辟了一块区域存放运行时常量池。
+//        因此intern()返回的引用和由StringBuilder创建的那个字符串实例是同一个。
+//        对 str2的比较返回false是因为“java”这个字符串在执行 StringBuilder.toString()之前已经出现过，
+//        字符串常量池中已经有它的引用了，不符合“首次出现”的原则，而“i'm T”这个字符串则是首次出现的，因此返回true。
+//
+//        链接：https://www.jianshu.com/p/5fdf752efc06
     }
 }
